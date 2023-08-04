1f3a91148d71a1f8a17184c510d9f8f4a89d1a7e
Nacho Lopez <nacho@nlopez.io>
2023-08-05 00:55:30 +0200

    Added detekt and ktlint versions with tests

--- rules/common/src/main/kotlin/io/nlopez/compose/rules/ComposeModifierNotUsedAtRoot.kt
+++ rules/common/src/main/kotlin/io/nlopez/compose/rules/ComposeModifierNotUsedAtRoot.kt
@@ -7,12 +7,15 @@ import io.nlopez.rules.core.Emitter
 import io.nlopez.rules.core.report
 import io.nlopez.rules.core.util.emitsContent
 import io.nlopez.rules.core.util.findChildrenByClass
-import io.nlopez.rules.core.util.isUsingModifiers
 import io.nlopez.rules.core.util.modifierParameter
 import io.nlopez.rules.core.util.obtainAllModifierNames
+import io.nlopez.rules.core.util.rootExpression
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+import org.jetbrains.kotlin.psi.KtValueArgument
 
 class ComposeModifierNotUsedAtRoot : ComposeKtVisitor {
 
@@ -23,25 +26,48 @@ class ComposeModifierNotUsedAtRoot : ComposeKtVisitor {
 
         val modifiers = code.obtainAllModifierNames("modifier")
 
-        val callExpressions = code.findChildrenByClass<KtCallExpression>()
+        val errors = code.findChildrenByClass<KtCallExpression>()
             .filter { it.calleeExpression?.text?.first()?.isUpperCase() == true }
-            .filter { it.isUsingModifiers(modifiers) }
-            .filter { callExpression ->
+            .mapNotNull { callExpression ->
+                val usage = callExpression.getModifierUsage(modifiers) ?: return@mapNotNull null
+                callExpression to usage
+            }
+            .filter { (callExpression, _) ->
                 // we'll need to traverse upwards to the composable root and check if there is any parent that
                 // emits content: if this is the case, the main modifier should be used there instead.
                 callExpression.findFirstAncestorEmittingContent(stopAt = code) != null
             }
+            .map { (_, valueArgument) -> valueArgument }
 
-        for (callExpression in callExpressions) {
-            emitter.report(callExpression, ComposableModifierShouldBeUsedAtTheTopMostPossiblePlace)
+        for (valueArgument in errors) {
+            emitter.report(valueArgument, ComposableModifierShouldBeUsedAtTheTopMostPossiblePlace)
         }
     }
 
+    private fun KtCallExpression.getModifierUsage(modifierNames: List<String>): KtValueArgument? =
+        valueArguments.firstOrNull { argument ->
+            when (val expression = argument.getArgumentExpression()) {
+                // if it's MyComposable(modifier) or similar
+                is KtReferenceExpression -> {
+                    modifierNames.contains(expression.text)
+                }
+                // if it's MyComposable(modifier.fillMaxWidth()) or similar
+                is KtDotQualifiedExpression -> {
+                    // On cases of multiple nested KtDotQualifiedExpressions (e.g. multiple chained methods)
+                    // we need to iterate until we find the start of the chain
+                    modifierNames.contains(expression.rootExpression.text)
+                }
+
+                else -> false
+            }
+        }
+
     private fun KtCallExpression.findFirstAncestorEmittingContent(stopAt: PsiElement): KtCallExpression? {
+        val origin = this
         var current: PsiElement = this
         var result: KtCallExpression? = null
         while (current != stopAt) {
-            if (current is KtCallExpression && current.emitsContent) {
+            if (current != origin && current is KtCallExpression && current.emitsContent) {
                 result = current
             }
             current = current.parent
