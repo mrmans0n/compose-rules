8a89493bb37c7d396ff276466375b869abe63a68
Nacho Lopez <nacho.lopez@thetradedesk.com>
2024-10-03 16:00:09 +0200

    Fix KtTypeReference#isComposableLambda

--- rules/ktlint/src/test/kotlin/io/nlopez/compose/rules/ktlint/ContentSlotReusedCheckTest.kt
+++ rules/ktlint/src/test/kotlin/io/nlopez/compose/rules/ktlint/ContentSlotReusedCheckTest.kt
@@ -80,24 +80,29 @@ class ContentSlotReusedCheckTest {
         val code =
             """
                 @Composable
-                fun A(text: String, content: (@Composable () -> Unit)? = null) {
+                fun A(text: String, content: @Composable (() -> Unit)? = null) {
                     if (x) content?.invoke() else content?.invoke()
                 }
                 @Composable
-                fun B(text: String, content: (@Composable () -> Unit)? = null) {
+                fun B(text: String, content: @Composable (() -> Unit)? = null) {
                     when {
                         x -> content?.invoke()
                         else -> content?.invoke()
                     }
                 }
                 @Composable
-                fun C(text: String, content: (@Composable () -> Unit)? = null) {
+                fun C(text: String, content: @Composable (() -> Unit)? = null) {
                     val content1 = remember { movableContentOf { content?.invoke() } }
                     val content2 = remember { movableContentOf { content?.invoke() } }
+                }
+                @Composable
+                fun D(content: Potato? = null) {
+                    if (x) content?.invoke() else content?.invoke()
                 }
             """.trimIndent()
 
         ruleAssertThat(code)
+            .withEditorConfigOverride(treatAsComposableLambda to "Potato", treatAsLambda to "Plum")
             .hasLintViolationsWithoutAutoCorrect(
                 LintViolation(
                     line = 2,
@@ -112,6 +117,11 @@ class ContentSlotReusedCheckTest {
                 LintViolation(
                     line = 13,
                     col = 21,
+                    detail = ContentSlotReused.ContentSlotsShouldNotBeReused,
+                ),
+                LintViolation(
+                    line = 18,
+                    col = 7,
                     detail = ContentSlotReused.ContentSlotsShouldNotBeReused,
                 ),
             )
@@ -145,6 +155,14 @@ class ContentSlotReusedCheckTest {
                 }
                 fun B(content: Plum, text: String) {
                     if (x) content() else content()
+                }
+                @Composable
+                fun C(content: (() -> Unit)? = null) {
+                    if (x) content?.invoke() else content?.invoke()
+                }
+                @Composable
+                fun D(content: Plum? = null) {
+                    if (x) content?.invoke() else content?.invoke()
                 }
             """.trimIndent()
 
