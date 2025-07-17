07bd2fb319a6d9f0b1e5f6f2ef64d9ac206cd215
Nacho Lopez <nacho@nlopez.io>
2025-07-17 16:12:11 +0200

    Support separate kotlin compiler versions for detekt and ktlint (#475)

---
+++ rules/common-detekt/build.gradle.kts
@@ -0,0 +1,23 @@
+// Copyright 2024 Nacho Lopez
+// SPDX-License-Identifier: Apache-2.0
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+dependencies {
+    compileOnlyOrApi(libs.kotlin.compiler.detekt)
+
+    testImplementation(libs.junit5)
+    testImplementation(libs.junit5.params)
+    testImplementation(libs.assertj)
+}
+
+// Include the source code from the main common module
+sourceSets {
+    main {
+        kotlin.srcDirs("../common/src/main/kotlin")
+    }
+    test {
+        kotlin.srcDirs("../common/src/test/kotlin")
+    }
+}
