1de8d54862dc767fbe9b329523cc25393f0adcfe
Albert Chang <mxalbert1996@gmail.com>
2026-01-21 20:56:54 +0900

    Add shadowJar relocate rule

--- rules/ktlint/build.gradle.kts
+++ rules/ktlint/build.gradle.kts
@@ -18,11 +18,12 @@ if (!project.hasProperty("uberJar")) {
 
 tasks.shadowJar {
     // Relocate packages that may conflict with the ones IntelliJ IDEA provides as well.
-    // See https://github.com/nbadal/ktlint-intellij-plugin/blob/main/lib/build.gradle.kts
+    // See https://github.com/nbadal/ktlint-intellij-plugin/blob/main/ktlint-lib/build.gradle.kts
     relocate("org.jetbrains.concurrency", "shadow.org.jetbrains.concurrency")
     relocate("org.jetbrains.kotlin.psi.KtPsiFactory", "shadow.org.jetbrains.kotlin.psi.KtPsiFactory")
     relocate("org.jetbrains.kotlin.psi.psiUtil", "shadow.org.jetbrains.kotlin.psi.psiUtil")
     relocate("org.jetbrains.org", "shadow.org.jetbrains.org")
+    relocate("org.jetbrains.kotlin", "shadow.org.jetbrains.kotlin")
 }
 
 dependencies {
